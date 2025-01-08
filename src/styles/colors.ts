0af4bc033980caeeae7ef4b490da0497ef7baff2
DeoKanKoong <rla9046@gmail.com>
2025-01-09 03:38:00 +0900

    default

---
+++ src/styles/colors.ts
@@ -0,0 +1,40 @@
+const colors = {
+    point: {
+      normal: "#99CDFF",        // blue-100
+      alternative: "#4EACFF",   // blue-200
+      hero: "#0283F5",          // blue-300
+      assistive: "#004E98",     // blue-400
+      navy: "#001427",          // blue-500
+      ivory: "#FDFDF8",         // ivory-100
+      yellow: "#FFFFF1",        // ivory-200
+      neon: "#00E5FF",          // neon-100
+      gray: "#393940",          // gray-600
+    },
+    object: {
+      normal: "#F3F3F3",        // gray-100
+      alternative: "#D4D4D4",   // gray-200
+      hero: "#919191",          // gray-400
+      assistive: "#767676",     // gray-500
+    },
+    border: {
+      normal: "#70737C",        // transparent-100
+      alternative: "#919191",   // transparent-200
+    },
+    feedback: {
+      positive: "#00AF3B",      // green-100
+      caution: "#FFB149",       // yellow-100
+      negative: "#FF5656",      // red-100
+    },
+    base: {
+      white: "#FFFFFF",         // white-100
+      darkgray: "#2B2B2B",      // gray-700
+      black: "#000000",         // black-100
+    },
+    interactive: {
+      disable: "#F3F3F3",       // gray-100
+      inactive: "#989BA2",      // gray-300
+    },
+  };
+  
+  export default colors;
+  
