9178e51bf5874edc8bc1c833bb913dfc85399cf5
DeoKanKoong <rla9046@gmail.com>
2025-01-09 09:23:33 +0900

    default

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-function App() {
+/** @jsxImportSource @emotion/react */
+
+const App = () => {
   return (
     <div>
-      <h1>Welcome to the App</h1>
     </div>
   );
-}
+};
 
 export default App;
