1cf2c55a24f656708f9901f4b17b533f867f494b
Sohyunnnn <thgus05061@naver.com>
2025-01-09 19:43:10 +0900

    remove: 불필요한 코드 및 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { Global, ThemeProvider } from "@emotion/react";
 import theme from "./styles/theme";
-import { css } from "@emotion/react";
-import { globalStyles } from "./styles/global";
+import { globalStyles } from "@styles/global";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Global styles={globalStyles} />
-      <p
-        css={css`
-          background-color: ${theme.color.feedback.negative};
-          padding: 20px;
-          ${theme.font.display4};
-        `}
-      >
-        hello world!
-      </p>
     </ThemeProvider>
   );
 };
--- src/styles/emotion.d.ts
+++
@@ -1,9 +0,0 @@
-import "@emotion/react";
-import { ColorType, FontType } from "./theme";
-
-declare module "@emotion/react" {
-  export interface Theme {
-    color: ColorType;
-    font: FontType;
-  }
-}
