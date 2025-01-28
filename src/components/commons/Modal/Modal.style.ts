83212430f706df1d6a5810a87e22a4f5a69b059f
숨 <107913402+smin7@users.noreply.github.com>
2025-01-28 21:00:53 +0900

    [Refactor/#46] 공통 모달창 수정 및 게시물 수정 드롭다운 제작 (#57)
    * style: 공통모달 수정
    
    * asset: 수정하기 드롭다운 아이콘 추가
    
    * asset: 수정하기 드롭다운 추가
    
    * feature: 수정하기 드롭다운 구현
    
    * remove: dropdownIcon 파일 삭제
    
    * style : 공통 모달 스타일 수정
    
    * refactor: 스타일 수정 및 onClick 기능 추가
    
    * refactor: 메세지 설정할 수 있도록 수정
    
    * refactor: 코드 삭제 및 스타일 코드 간결화
    
    * test: 드롭다운 테스트한 후 복구
    
    * refactor: 모달 및 myfeed드롭다운 수정

--- src/components/commons/Modal/Modal.style.ts
+++ src/components/commons/Modal/Modal.style.ts
@@ -1,65 +1,59 @@
 import { css } from "@emotion/react";
 import theme from "@styles/theme";
 
-export const modalStyles = {
-  background: css`
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0, 0, 0, 0.40);
-  `,
-  container: css`
-    width: 50rem;
-    height: 30.4rem;
-    background: ${theme.color.base.white};
-    box-shadow: 0rem 0rem 0.1rem rgba(0, 0, 0, 0.04);
-    border: 0.1rem solid ${theme.color.object.alternative};
-    border-radius: 2.4rem;
-    display: inline-flex;
-    flex-direction: column;
-  `,
-  header: css`
-    padding: 1.6rem 2.4rem;
-    display: flex;
-    justify-content: end;
-    align-items: center;
-    cursor: pointer;
-  `,
-  body: css`
-    width: 50rem;
-    height: 15rem;
-    padding: 1.6rem 2.4rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 1.6rem;
-  `,
+export const container = css`
+  width: 54rem;
+  padding: 3.2rem;
+  background: ${theme.color.base.white};
+  box-shadow: 0rem 0rem 0.1rem rgba(0, 0, 0, 0.04);
+  border: 0.1rem solid ${theme.color.object.alternative};
+  border-radius: 2.4rem;
+  display: inline-flex;
+  flex-direction: column;
+`;
+export const header = css`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  cursor: pointer;
+`;
 
-  footer: css`
-    width: 50rem;
-    height: 9.4rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 1rem 0 1rem 0;
-    gap: 3rem;
-  `,
+export const body = css`
+  width: 47.6rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.6rem;
+`;
 
-  button: (backgroundColor: string) => css`
-    width: 19.4rem;
-    height: 5rem;
-    padding: 0.6rem 2.4rem;
-    background: ${backgroundColor};
-    color: ${theme.color.point.ivory};
-    border-radius: 1.6rem;
-    border: 0.1rem solid rgba(112, 115, 124, 0.2);
-    ${theme.font.caption3};
-    cursor: pointer;
-  `,
-};
+export const titleStyle = css`
+  color: ${theme.color.base.darkgray};
+  ${theme.font.title1};
+  margin-top: 1.6rem;
+`;
+
+export const messageStyle = css`
+  color: ${theme.color.object.assistive};
+  ${theme.font.caption3};
+`;
+export const footer = css`
+  margin-top: 1.6rem;
+  height: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.6rem;
+`;
+
+export const button = (backgroundColor: string) => css`
+  width: 19.4rem;
+  height: 5rem;
+  padding: 0.6rem 2.4rem;
+  background: ${backgroundColor};
+  color: ${theme.color.point.ivory};
+  border-radius: 1.6rem;
+  border: 0.1rem solid rgba(112, 115, 124, 0.2);
+  ${theme.font.caption3};
+  cursor: pointer;
+`;
