3ebe09f1cd3ce85093d0959d8637bd46d83ee944
Sohyunnnn <108279922+Sohyunnnn@users.noreply.github.com>
2025-02-19 01:10:52 +0900

    fix: 로그아웃 오류 수정 (#147)

--- src/apis/hooks/users/userPostLogout.ts
+++ src/apis/hooks/users/userPostLogout.ts
@@ -1,6 +1,6 @@
 import routes from "@constants/routes";
 import { useMutation } from "@tanstack/react-query";
-import { postNickname } from "apis/userAPI";
+import { postLogout } from "apis/userAPI";
 import { useNavigate } from "react-router-dom";
 import { useSnackbar } from "providers";
 
@@ -9,7 +9,7 @@ const usePostLogout = () => {
   const { showSnackbar } = useSnackbar();
 
   return useMutation({
-    mutationFn: (refreshToken: string) => postNickname(refreshToken),
+    mutationFn: (refreshToken: string) => postLogout(refreshToken),
     onSuccess: () => {
       navigate(routes.home);
       localStorage.removeItem("accessToken");
