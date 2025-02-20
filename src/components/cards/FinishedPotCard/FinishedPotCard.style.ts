dd510d30f447c3ac258cdf39df1362de888c5c09
숨 <107913402+smin7@users.noreply.github.com>
2025-02-21 01:32:47 +0900

    style: 밀림 현상 방지 (#180)

--- src/components/cards/FinishedPotCard/FinishedPotCard.style.ts
+++ src/components/cards/FinishedPotCard/FinishedPotCard.style.ts
@@ -2,52 +2,57 @@ import { css } from "@emotion/react";
 import theme from "@styles/theme";
 
 export const container = css`
-    display: flex;
-    flex-direction: column;
-    gap: 3.2rem;
-    padding: 3.8rem;
-    border-radius: 24px;
-    border: 1px solid ${theme.color.object.alternative};
-    box-shadow: 0px 4px 12px 0px rgba(13, 10, 44, 0.06);
-    cursor: pointer;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+  padding: 3.8rem;
+  border-radius: 24px;
+  border: 1px solid ${theme.color.object.alternative};
+  box-shadow: 0px 4px 12px 0px rgba(13, 10, 44, 0.06);
+  cursor: pointer;
+`;
 export const titleContainer = css`
-    display: flex;
-    justify-content: space-between;
-    height: 4rem;
-    align-items: center;
-`
+  display: flex;
+  justify-content: space-between;
+  height: 4rem;
+  align-items: center;
+`;
 export const titleProfileContainer = css`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 export const profileContainer = css`
-    display: flex;
-`
+  display: flex;
+`;
 export const titleStyle = css`
-    ${theme.font.title1};
-    color: ${theme.color.base.darkgray};
-`
+  ${theme.font.title1};
+  color: ${theme.color.base.darkgray};
+`;
 
 export const gridContainer = css`
-    display: grid;
-    grid-template-columns: max-content max-content;
-    column-gap: 17rem;
-    row-gap: 1.8rem;
-`
+  display: grid;
+  grid-template-columns: 30rem 30rem;
+  column-gap: 13rem;
+  row-gap: 1.8rem;
+`;
 export const elementContainer = css`
-    display: flex;
-    gap: 2.4rem;
-`
+  display: flex;
+  gap: 2.4rem;
+`;
 export const teamElementContainer = css`
-    display: flex;
-    gap: 4rem;
-`
+  display: flex;
+  gap: 4rem;
+`;
 export const elementTitleStyle = css`
-    ${theme.font.body1}
-    color: ${theme.color.interactive.inactive};
-`
+  ${theme.font.body1}
+  white-space: nowrap;
+
+  color: ${theme.color.interactive.inactive};
+`;
 export const elementContentStyle = css`
-    ${theme.font.body1};
-    color: ${theme.color.base.darkgray};
-`
+  ${theme.font.body1};
+  color: ${theme.color.base.darkgray};
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
